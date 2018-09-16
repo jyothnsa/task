e211822fc3f9eaf592f35f71f532250bd5ec7e5d
jyothnsa <jyothsnagampa@gmail.com>
2018-09-16 22:42:39 +0530

    changed main file

--- employeedata/src/App.js
+++ employeedata/src/App.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import sampleData from './sample-data.json';
 class App extends Component {
-constructor(props){
-  super(props);
-}
+
   render() {
     console.log(sampleData);
 
     return (
-      <div>
-      <h1>{sampleData.companyInfo.companyName}  </h1>
+      <div className="App">
+      <header>
+      <h2>{sampleData.companyInfo.companyName}</h2>
+      <h4>{sampleData.companyInfo.companyMotto}</h4>
+      </header>
+      <p>
+      <h3 style = {{padding: 10,fontSize: 30}}> Employees Data </h3>
+      <div className="gridContainer">
+
+        {sampleData.employees.map((data,i) => <div className="grid-item">
+       <p><span>{data.firstName} {data.lastName} </span><span><img src={data.avatar} alt="employeeImg"/></span>
+
+                     <p><b>Age:</b> {data.age} </p>
+                     <p><b>JobDesc:</b> {data.jobTitle}</p>
+                     <p>{data.bio}</p>
+
+</p>
+                        </div> )}
+</div>
+</p>
       </div>
     );
   }
